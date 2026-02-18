26149bd59be3343a46e4e7bc96ce8d37b365ebb0
shunitron <shunitron@example.com>
2026-02-18 11:03:50 +0900

    refactor: matchの冗長的な部分を早期リターンに修正

--- src/main.rs
+++ src/main.rs
@@ -40,32 +40,31 @@ impl Blob {
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         println!("使用法: cargo run -- <ファイル名>");
-        return;
+        return Ok(());
     }
 
     let filename = &args[1];
 
-    match fs::read_to_string(filename) {
-        Ok(content) => {
-            let blob = Blob::new(filename, &content);
-            let obj = GitObject::Blob(blob);
-            obj.print_type();
-            match obj {
-                GitObject::Blob(inner_blob) => {
-                    inner_blob.display();
-                    if let Err(e) = save_object(&inner_blob.hash, &inner_blob.content) {
-                        eprint!("保存失敗: {}", e);
-                    }
-                }
-            }
-        }
-        Err(e) => eprintln!("エラー: {}", e),
+    // 1. ファイル読み込みに失敗したら即エラーを返して終了
+    let content = fs::read_to_string(filename)?;
+
+    let blob = Blob::new(filename, &content);
+    let obj = GitObject::Blob(blob);
+
+    if let GitObject::Blob(inner_blob) = obj {
+        println!("ファイル名: {}", inner_blob.filename);
+        println!("ハッシュ: {}", inner_blob.hash);
+
+        save_object(&inner_blob.hash, &inner_blob.content)?;
+        println!("保存成功！");
     }
+
+    Ok(())
 }
 
 fn save_object(hash: &str, content: &str) -> std::io::Result<()> {
@@ -74,8 +73,6 @@ fn save_object(hash: &str, content: &str) -> std::io::Result<()> {
     let file_path = format!("{}/{}", dir_path, file_name);
 
     fs::create_dir_all(&dir_path)?;
-
     fs::write(file_path, content)?;
-
     Ok(())
 }
